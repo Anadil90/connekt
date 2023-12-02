fe1cb647dec4ecfe73fa6ea3bc29af57e8315fcc
Anadil90 <45528659+Anadil90@users.noreply.github.com>
2023-12-02 15:57:47 +0100

    secure password for user registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,25 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 router.get('/', (req, res) => {
     res.send('user authentication route');
 })
 
 router.post('/register', async(req, res) => {
-    const newUser = await new User({
+
+    const salt = await bcrypt.genSalt(12); //generate random alphanumeric number 
+    const hashedPassword = await bcrypt.hash(req.body.password, salt); //hash password with generated salt for security
+    
+    //create new user
+    const newUser = new User({
         username: req.body.username,
-        password: req.body.password,
+        password: hashedPassword,
         email: req.body.email
     })
 
     try {
+        //save user
         const user = await newUser.save()
         res.status(200).json(user);
     }
