bb49c227e729f1897276504bae51d6c28afbd982
Anadil90 <45528659+Anadil90@users.noreply.github.com>
2023-12-07 20:03:14 +0100

    User follow and unfollow

--- routes/users.js
+++ routes/users.js
@@ -31,7 +31,6 @@ router.put('/:id', async(req, res) => {
         return res.status(403).json("You can update only your account!");
     }
 });
-
 //delete user
 router.delete('/:id', async(req, res) => {
     if(req.body.userId === req.params.id || req.body.adminPrivelages) {
@@ -48,6 +47,61 @@ router.delete('/:id', async(req, res) => {
     }
 });
 //get a user
+router.get('/:id', async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id);
+        const {password, updatedAt, ...other} = user._doc
+        res.status(200).json(other);
+    }
+    catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 //follow user
+router.put('/:id/follow', async (req, res) => {
+    if(req.body.userId === req.params.id) {
+        try {
+            const user = await User.findById(req.params.id)
+            const currentUser = await User.findById(req.body.userId)
+            if(!user.followers.includes(req.body.userId)) {
+                await user.updateOne({$push: { followers: req.body.userId}});
+                await currentUser.updateOne({$push: {following: req.params.id}});
+                res.status(200).json(`you are following this user`);
+
+            }
+            else {
+                res.status(403).json('You are already following this user!');
+            }
+
+        }    
+        catch(err) {
+            res.status(500).json(err)
+        }
+    }
+})
+
+//unfollow a user
+router.put('/:id/unfollow', async (req, res) => {
+    if(req.body.userId === req.params.id) {
+        try {
+            const user = await User.findById(req.params.id)
+            const currentUser = await User.findById(req.body.userId)
+            if(!user.followers.includes(req.body.userId)) {
+                await user.updateOne({$pull: { followers: req.body.userId}});
+                await currentUser.updateOne({$pull: {following: req.params.id}});
+                res.status(200).json(`you are no longer following this user`);
+
+            }
+            else {
+                res.status(403).json('You already are not following this user!');
+            }
+
+        }    
+        catch(err) {
+            res.status(500).json(err)
+        }
+    }
+})
+
 module.exports = router;
